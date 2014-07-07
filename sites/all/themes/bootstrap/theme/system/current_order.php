410ecf0dd79daf925118b2edf9954fc860d28d32
issa.diao <issa@everyconcertever.com>
2014-07-06 22:00:52 -0700

    order session data

--- sites/all/themes/bootstrap/theme/system/current_order.php
+++ sites/all/themes/bootstrap/theme/system/current_order.php
@@ -1,7 +1,12 @@
 <?php if (isset($_SESSION['order'])) { ?>
   <div class="col-sm-12">
+    <table>
+      <tr><th>Item</th><th>Quantity</th></tr>
     <?php
-    print_r(orderSession::displayItems());
+    foreach (orderSession::displayItems() as $item) {
+      echo "<tr><td>" . $item['title'] . "</td><td>" . $item['quantity'] . "</td></tr>";
+    }
     ?>
+    </table>
   </div>
 <?php } ?>
