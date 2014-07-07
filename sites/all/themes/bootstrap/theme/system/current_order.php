64c047c97bbd04363cfff153e5fa752baff0d9c9
issa.diao <issa@everyconcertever.com>
2014-07-06 22:03:04 -0700

    order session data

--- sites/all/themes/bootstrap/theme/system/current_order.php
+++ sites/all/themes/bootstrap/theme/system/current_order.php
@@ -1,12 +1,14 @@
 <?php if (isset($_SESSION['order'])) { ?>
   <div class="col-sm-12">
-    <table>
-      <tr><th>Item</th><th>Quantity</th></tr>
+    <table class="table table-striped table-bordered table-condensed">
+      <thead><tr><th>Item</th><th>Quantity</th></tr></thead>
+      <tbody>
     <?php
     foreach (orderSession::displayItems() as $item) {
       echo "<tr><td>" . $item['title'] . "</td><td>" . $item['quantity'] . "</td></tr>";
     }
     ?>
+      </tbody>
     </table>
   </div>
 <?php } ?>
