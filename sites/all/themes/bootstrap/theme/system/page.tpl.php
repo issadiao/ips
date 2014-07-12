e8b772ab0c6867d06bc379eeda5f99778f049eab
issa.diao <issa@everyconcertever.com>
2014-07-11 17:42:18 -0700

    footer

--- sites/all/themes/bootstrap/theme/system/page.tpl.php
+++ sites/all/themes/bootstrap/theme/system/page.tpl.php
@@ -105,7 +105,7 @@ include_once('header.php');
       <?php if (!empty($page['highlighted'])): ?>
         <div class="highlighted jumbotron"><?php print render($page['highlighted']); ?></div>
       <?php endif; ?>
-      <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+
       <a id="main-content"></a>
       <?php print render($title_prefix); ?>
       <?php if (!empty($title)): ?>
