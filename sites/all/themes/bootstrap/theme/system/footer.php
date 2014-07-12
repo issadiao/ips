87b6c521a0c13796bbcb01eaaaa2830cdd95e2a3
issa.diao <issa@everyconcertever.com>
2014-07-11 17:28:41 -0700

    footer

--- sites/all/themes/bootstrap/theme/system/footer.php
+++ sites/all/themes/bootstrap/theme/system/footer.php
@@ -6,6 +6,6 @@
     </nav>
   </div>
 
-  <ul class="credits col-sm-12 col-md-6"><li>Washington D.C.</li><li>Connecticut</li><li>Los Angeles</li></ul>
+  <ul class="credits col-sm-12 col-md-6"><li>Washington D.C.</li><li>Hartford</li><li>Los Angeles</li></ul>
 
 </footer>
