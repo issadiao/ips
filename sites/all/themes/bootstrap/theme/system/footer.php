4977e79d025165f10a8b6fffceef5d8e00dd04e5
issa.diao <issa@everyconcertever.com>
2014-07-11 17:23:23 -0700

    footer

--- sites/all/themes/bootstrap/theme/system/footer.php
+++ sites/all/themes/bootstrap/theme/system/footer.php
@@ -1,11 +1,11 @@
 <footer class="footer container">
 
-  <div class="footer-nav">
+  <div class="footer-nav col-xs-6">
     <nav role="navigation">
       <?php print render($page['navigation']); ?>
     </nav>
   </div>
 
-  <ul class="credits"><li>Los Angeles</li><li>Connecticut</li><li>Washington D.C.</li></ul>
+  <ul class="credits col-xs-6"><li>Los Angeles</li><li>Connecticut</li><li>Washington D.C.</li></ul>
 
 </footer>
