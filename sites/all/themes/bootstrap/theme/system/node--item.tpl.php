672395724937a41fbc227ecbe4421b27bd226558
issa diao <issa@marie-diaos-macbook.local>
2014-07-17 12:29:37 -0700

    repair

--- sites/all/themes/bootstrap/theme/system/node--item.tpl.php
+++ sites/all/themes/bootstrap/theme/system/node--item.tpl.php
@@ -13,7 +13,9 @@ if ($node->field_item_needs_repair['und'][0]['value'] == "no") {
 ?>
 
   <div class="repair row col-xs-12">
-  This item is in need of repair. See details here: <?php echo ips_repairs_item_repair_link($node->nid); ?>
+    <div class="container">
+      This item is in need of repair. See details here: <?php echo ips_repairs_item_repair_link($node->nid); ?>
+    </div>
   </div>
 
 <?php } ?>
