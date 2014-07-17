7307bedfc47605a10419c3b361cdd6582591f8c0
issa diao <issa@marie-diaos-macbook.local>
2014-07-17 11:48:57 -0700

    repair

---
+++ sites/all/themes/bootstrap/theme/system/node--repair.tpl.php
@@ -0,0 +1,39 @@
+<?php
+
+if ($node->field_repair_complete['und'][0]['value'] == "no") {
+  $completed_form = drupal_get_form('ips_repairs_completed_form', $node);
+  echo drupal_render($completed_form);
+}
+
+?>
+
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?> clearfix"<?php print $attributes; ?>>
+
+  <?php print $user_picture; ?>
+
+  <?php print render($title_prefix); ?>
+  <?php if (!$page): ?>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+
+  <?php if ($display_submitted): ?>
+    <div class="submitted">
+      <?php print $submitted; ?>
+    </div>
+  <?php endif; ?>
+
+  <div class="content"<?php print $content_attributes; ?>>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+
+  <?php print render($content['links']); ?>
+
+  <?php print render($content['comments']); ?>
+
+</div>
